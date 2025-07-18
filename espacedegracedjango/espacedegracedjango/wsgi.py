b11af7cab9d8272421f97d15999ca7576d003c8d
GLystein <47366109+GLystein@users.noreply.github.com>
2025-07-18 00:52:39 -0500

    redoing the setup

--- espacedegracedjango/espacedegracedjango/wsgi.py
+++ espacedegracedjango/espacedegracedjango/wsgi.py
@@ -10,6 +10,6 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'espacedegracedjango.settings')
-os.environ ["DJANGO_SETTINGS_MODULE"] = "espacedegracedjango.settings"
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'espacedegracedjango.settings')
+# os.environ ["DJANGO_SETTINGS_MODULE"] = "espacedegracedjango.settings"
 application = get_wsgi_application()
