21de3c22788a11ebecb203d1d50447ca9490f5ad
GLystein <47366109+GLystein@users.noreply.github.com>
2025-08-14 04:12:14 -0500

    update the folder name

--- espacedegracedjango/espacedegrace/wsgi.py
+++ espacedegracedjango/espacedegrace/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for espacedegracedjango project.
+WSGI config for espacedegrace project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -10,6 +10,6 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'espacedegracedjango.settings')
-# os.environ ["DJANGO_SETTINGS_MODULE"] = "espacedegracedjango.settings"
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'espacedegrace.settings')
+# os.environ ["DJANGO_SETTINGS_MODULE"] = "espacedegrace.settings"
 application = get_wsgi_application()
